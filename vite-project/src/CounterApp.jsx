bb343657fa3bd0e0e822ae763ddc93496c69dbd6
jaehokang1007 <jaehokang1007@icloud.com>
2026-04-03 09:41:31 +0900

    CounterApp +2, -, reset

--- vite-project/src/CounterApp.jsx
+++ vite-project/src/CounterApp.jsx
@@ -12,12 +12,10 @@ function CounterApp() {
     <>
       <h1>{count}</h1>
       <button onClick={() => setCount((count) => count + 1)}>+</button>
-      {/* <button
-        className="counter"
-        onClick={() => setCount((count) => count + 1)}
-      >
-        Count is {count}
-      </button> */}
+      <button onClick={() => setCount((count) => count + 2)}>+2</button>
+      <button onClick={() => setCount((count) => count - 1)}>-</button>
+      <button onClick={() => setCount((0))}>reset</button>
+      {/* <button onClick={setCount(0)}>reset</button> */}
     </>
   );
 }
